cf4d6a639c503f9c7dd8d71018a4948678a72a8c
Percdye7 <percdye7@gmail.com>
2016-08-06 17:20:34 +0200

    Update Settings.vb

--- Settings.vb
+++ Settings.vb
@@ -1,6 +1,18 @@
-﻿Public Class Settings
+Public Class Settings
     Dim loc As Point
     Dim loc2 As Point
+
+#Region "Check Network Connection"
+    Function CheckNetworkConnection()
+        Try
+            My.Computer.Network.Ping("www.google.de")
+            Return 1
+        Catch ex As Exception
+            Return 0
+        End Try
+
+    End Function
+#End Region
 
     Private Sub Settings_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -31,5 +43,16 @@
         End If
     End Sub
 #End Region ' Giving the Form the ability to get Moveable by the Mouse
+
+    Private Sub CheckNet_Click(sender As Object, e As EventArgs) Handles CheckNet.Click
+        If CheckNetworkConnection() = 1 Then
+            MsgBox("Your Network works Fine :^)", MsgBoxStyle.Information, "Network Connection")
+        Else
+            If CheckNetworkConnection() = 0 Then
+                MsgBox("You are not Connected with the Internet, please check your connection.", MsgBoxStyle.Critical, "Error: No Network Connection!")
+                Application.Exit()
+            End If
+        End If
+    End Sub
 
 End Class
